65a2cc6014b5a364614301eec64d0bed9a67c8a3
dopplegangerK <kelsey.pownell@gmail.com>
2019-02-10 12:55:36 -0600

    Attach health bar to BossMonsterBase class

--- Source/Pact/Private/BossMonsterBase.cpp
+++ Source/Pact/Private/BossMonsterBase.cpp
@@ -1,30 +1,72 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BossMonsterBase.h"
-#include <iostream>
+#include "Components/WidgetComponent.h"
+#include "Blueprint/UserWidget.h"
+#include "UObject/ConstructorHelpers.h"
+
 
 // Sets default values
 ABossMonsterBase::ABossMonsterBase()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	OnTakeAnyDamage.AddDynamic(this, &ABossMonsterBase::handleDamage);
+	// Find the BossHealthBar class
+	ConstructorHelpers::FClassFinder<UUserWidget> HealthBarClassFinder(TEXT("/Game/GameObjects/Widgets/BossHealthBar"));
+	if (!HealthBarClassFinder.Succeeded()) {
+		UE_LOG(LogTemp, Error, TEXT("Could not find BossHealthBar widget class"));
+		return;
+	}
+	HealthBarClass = HealthBarClassFinder.Class;
 }
 
 // Called when the game starts or when spawned
 void ABossMonsterBase::BeginPlay()
 {
 	Super::BeginPlay();
-	std::cout << "Hello\n";
+	defeated = false;
+	attacking = false;
+	health = 100;
+
+	// Create health bar
+	APlayerController* controller = GetWorld()->GetFirstPlayerController();
+	myHealthBar = CreateWidget<UUserWidget>(controller, HealthBarClass);
+	if (myHealthBar) {
+		// set visible
+		myHealthBar->AddToViewport();
+		// Set health bar's Boss property to this object
+		FName bossPropertyName(TEXT("Boss"));
+		UObjectProperty* bossProperty = FindField<UObjectProperty>(HealthBarClass, bossPropertyName);
+		if (bossProperty) {
+			bossProperty->SetPropertyValue_InContainer(myHealthBar, this);
+		}
+	}
 }
 
 // Called every frame
 void ABossMonsterBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 bool ABossMonsterBase::isAttacking() { return attacking; }
 
+bool ABossMonsterBase::isDefeated() { return defeated; }
+
 float ABossMonsterBase::getPlayerDamage() { return 0; }
+
+void ABossMonsterBase::handleDefeat() {
+	myHealthBar->RemoveFromParent();
+};
+
+void ABossMonsterBase::handleDamage(AActor* damagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser) {
+	if (!defeated) {
+		health -= Damage;
+		if (health <= 0) {
+			health = 0;
+			defeated = true;
+			handleDefeat();
+		}
+	}
+}
--- Source/Pact/Public/BossMonsterBase.h
+++ Source/Pact/Public/BossMonsterBase.h
@@ -16,7 +16,18 @@ public:
 	ABossMonsterBase();
 
 protected:
+	TSubclassOf<class UUserWidget> HealthBarClass;
+
+	UPROPERTY(BlueprintReadWrite)
+	UUserWidget* myHealthBar;
+
+	UPROPERTY(BlueprintReadWrite)
 	bool attacking;
+
+	UPROPERTY(BlueprintReadWrite)
+	bool defeated;
+
+	UPROPERTY(BlueprintReadWrite)
 	float health;
 
 	// Called when the game starts or when spawned
@@ -26,10 +37,22 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	UFUNCTION(BlueprintCallable, Category = "Boss")
-	virtual bool isAttacking();
+	// Handler for OnTakeAnyDamage event
+	UFUNCTION()
+	virtual void handleDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
 
+	// Called by handleDamage when health reaches 0 (override in children)
+	UFUNCTION(BlueprintCallable, Category = "Boss")
+	virtual void handleDefeat();
+
+	// Calculate damage to player on hit (override in children)
 	UFUNCTION(BlueprintCallable, Category = "Boss")
 	virtual float getPlayerDamage();
 
+	// Getters and setters
+	UFUNCTION(BlueprintCallable, Category = "Boss")
+	virtual bool isDefeated();
+
+	UFUNCTION(BlueprintCallable, Category = "Boss")
+	virtual bool isAttacking();
 };
